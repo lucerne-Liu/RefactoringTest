d30ee98c4aee8708ef7e9879528b63bc7f3c10e4
Lucerne <lucerne_liu@foxmail.com>
2018-04-08 18:36:05 +0800

    refactoring practice3

--- src/main/java/practice3/Order.java
+++ src/main/java/practice3/Order.java
@@ -15,25 +15,19 @@ public class Order {
         this.tax = new BigDecimal(0.1);
     }
 
+    public List<OrderLineItem> getOrderLineItemList() {
+        return orderLineItemList;
+    }
+
+    public List<BigDecimal> getDiscounts() {
+        return discounts;
+    }
+
+    public BigDecimal getTax() {
+        return tax;
+    }
+
     public BigDecimal calculate() {
-        BigDecimal subTotal = new BigDecimal(0);
-
-        // Total up line items
-        for (OrderLineItem lineItem : orderLineItemList) {
-            subTotal = subTotal.add(lineItem.getPrice());
-        }
-
-        // Subtract discounts
-        for (BigDecimal discount : discounts) {
-            subTotal = subTotal.subtract(discount);
-        }
-
-        // calculate tax
-        BigDecimal tax = subTotal.multiply(this.tax);
-
-        // calculate GrandTotal
-        BigDecimal grandTotal = subTotal.add(tax);
-
-        return grandTotal;
+        return new PriceCaculator(this).calculate();
     }
 }
--- src/main/java/practice3/PriceCaculator.java
+++ src/main/java/practice3/PriceCaculator.java
@@ -1,4 +1,45 @@
 package practice3;
 
+import java.math.BigDecimal;
+
 public class PriceCaculator {
+    private final Order _order;
+    private BigDecimal subTotal;
+    private BigDecimal tax;
+    private BigDecimal grandTotal;
+
+    public PriceCaculator(Order order) {
+        _order = order;
+        subTotal  = new BigDecimal(0);
+    }
+
+    public BigDecimal calculate() {
+        // Subtract discounts to get total price
+        subTotal = getTotalPrice().subtract(getDiscounts());
+
+        // calculate tax
+        tax = subTotal.multiply(_order.getTax());
+
+        // calculate GrandTotal
+        grandTotal = subTotal.add(tax);
+
+        return grandTotal;
+    }
+
+    private BigDecimal getDiscounts() {
+        BigDecimal result = tax = new BigDecimal(0);
+        for (BigDecimal discount : _order.getDiscounts()) {
+            result = result.add(discount);
+        }
+        return result;
+    }
+
+    // Total up line items
+    private BigDecimal getTotalPrice() {
+        BigDecimal result = tax = new BigDecimal(0);
+        for (OrderLineItem lineItem : _order.getOrderLineItemList()) {
+            result = result.add(lineItem.getPrice());
+        }
+        return result;
+    }
 }
